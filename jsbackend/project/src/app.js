798b96345b3a29a67b633cf6ed4daf4d5d71fa94
amaanansarii <amaanansari790786@gmail.com>
2026-01-12 21:27:47 +0530

    added api error and api response in utils

--- jsbackend/project/src/app.js
+++ jsbackend/project/src/app.js
@@ -1,7 +1,20 @@
 
 import express from "express"
+import cookieParser from "cookie-parser" //use to add and modify cookies for crud operations in cookies
+import cors from "cors"
 
-const app = express()
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, // origin = * which allows every origin if we want we can add specific origin.
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"})) // to access json limited
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+app.use(express.static("public"))
+app.use(cookieParser())
 
 
 export { app }
