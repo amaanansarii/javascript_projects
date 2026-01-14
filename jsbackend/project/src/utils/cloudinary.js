dac9a3498401280b6024fd9684d30ca3f62dc891
amaanansarii <amaanansari790786@gmail.com>
2026-01-14 22:08:58 +0530

    added cloudinary function

--- jsbackend/project/src/utils/cloudinary.js
+++ jsbackend/project/src/utils/cloudinary.js
@@ -8,3 +8,31 @@ cloudinary.config({
         api_key: process.env.CLOUDINARY_API_KEY, 
         api_secret: process.env.CLOUDINARY_API_SECRET
     });
+
+
+const uploadOnCloudindary = async (localFilePath => {
+    try {
+        if(!localFilePath) return null;
+        
+        //upload the file on cloudinary
+
+        cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded
+    } catch (error) {
+        
+    }
+})
+
+ const uploadResult = await cloudinary.uploader
+       .upload(
+           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+               public_id: 'shoes',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+       });
+    
+    console.log(uploadResult);
