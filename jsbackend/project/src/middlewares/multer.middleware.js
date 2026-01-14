8da5f408ac693a197a136fe001955b8b5fd13a4c
amaanansarii <amaanansari790786@gmail.com>
2026-01-14 22:45:15 +0530

    igmt

--- jsbackend/project/src/middlewares/multer.middleware.js
+++ jsbackend/project/src/middlewares/multer.middleware.js
@@ -5,9 +5,10 @@ const storage = multer.diskStorage({
         cb(null, "./public/temp")
     },
     filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname + '-' + uniqueSuffix)
+        cb(null, file.fieldname)
     }
 })
 
-export const upload = multer({storage,})
+export const upload = multer(
+    {storage,}
+)
