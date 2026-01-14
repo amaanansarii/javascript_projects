268c20f28f703a5bb1a3d8b62ceb5a9822207891
amaanansarii <amaanansari790786@gmail.com>
2026-01-14 22:38:21 +0530

    added multer

--- jsbackend/project/src/utils/cloudinary.js
+++ jsbackend/project/src/utils/cloudinary.js
@@ -29,14 +29,4 @@ const uploadOnCloudindary = async (localFilePath) => {
     }
 }
 
- const uploadResult = await cloudinary.uploader
-       .upload(
-           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-               public_id: 'shoes',
-           }
-       )
-       .catch((error) => {
-           console.log(error);
-       });
-    
-    console.log(uploadResult);
+export { uploadOnCloudindary }
