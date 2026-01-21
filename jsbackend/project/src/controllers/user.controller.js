789b35661a0274bd53196995b75bfa7ab165d995
amaanansarii <amaanansari790786@gmail.com>
2026-01-21 12:03:05 +0530

    added validation

--- jsbackend/project/src/controllers/user.controller.js
+++ jsbackend/project/src/controllers/user.controller.js
@@ -1,21 +1,6 @@
-// const generateAccessAndRefereshTokens = async(userId) =>{
-//     try {
-//         const user = await User.findById(userId)
-//         const accessToken = user.generateAccessToken()
-//         const refreshToken = user.generateRefreshToken()
-
-//         user.refreshToken = refreshToken
-//         await user.save({ validateBeforeSave: false })
-
-//         return {accessToken, refreshToken}
-
-
-//     } catch (error) {
-//         throw new ApiError(500, "Something went wrong while generating referesh and access token")
-//     }
-// }
-
 import { asyncHandler } from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
 
 const registerUser = asyncHandler( async (req, res) => {
 
@@ -41,7 +26,44 @@ const registerUser = asyncHandler( async (req, res) => {
     const {fullName, username, email, password} = req.body
     console.log("email",email)
 
+    //one of the method to validate but it can be validate using different method advanced method
+    // if(fullName === ""){
+    //     throw new ApiError(400, "full name is required")
+    // }
+
+    // for above method we will check for the if condition multiple times for every field but for below thing it will give the validation for each field in single array using some method.
+
+    if([fullName, email, username, password].some((field) => field?.trim() === "")){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    
+
     res.status(200).json({message: "ok"})
 });
 
 export { registerUser }
+
+
+
+
+// const generateAccessAndRefereshTokens = async(userId) =>{
+//     try {
+//         const user = await User.findById(userId)
+//         const accessToken = user.generateAccessToken()
+//         const refreshToken = user.generateRefreshToken()
+
+//         user.refreshToken = refreshToken
+//         await user.save({ validateBeforeSave: false })
+
+//         return {accessToken, refreshToken}
+
+
+//     } catch (error) {
+//         throw new ApiError(500, "Something went wrong while generating referesh and access token")
+//     }
+// }
