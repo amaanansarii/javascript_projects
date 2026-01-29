0c0586f5fd66884d99730b4afda61b265c5679e0
Amaan Ansari <amaanansari790786@gmail.com>
2026-01-29 22:46:00 +0530

    added new route

--- jsbackend/project/src/routes/user.route.js
+++ jsbackend/project/src/routes/user.route.js
@@ -1,6 +1,6 @@
 
 import { Router } from "express";
-import { logInUser, logOutUser, registerUser } from "../controllers/user.controller.js";
+import { logInUser, logOutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -24,5 +24,6 @@ userRouter.route("/login").post(logInUser)
 
 //secured route
 userRouter.route("/logout").post(verifyJwt, logOutUser)
+userRouter.route("/refresh-token").post(refreshAccessToken)
 
 export default userRouter;
