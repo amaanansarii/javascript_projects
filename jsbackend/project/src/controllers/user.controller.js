5ff97b06157578259db6750dd94b51c2c4e8a173
Amaan Ansari <amaanansari790786@gmail.com>
2026-01-30 06:47:00 +0530

    added update avatar controller

--- jsbackend/project/src/controllers/user.controller.js
+++ jsbackend/project/src/controllers/user.controller.js
@@ -275,7 +275,33 @@ const updateAccountDetails = asyncHandler( async(req, res) => {
     return res.status(200)
     .json(new ApiResponse(200, user, "Account Details updated successfully!!"))
 })
-export { registerUser, logInUser, logOutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails }
+
+const updateUserAvatar = asyncHandler( async(req, res) => {
+    const avatarLocalPath = req.file?.path
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is missing!")
+    }
+
+    const avatar = await uploadOnCloudindary(avatarLocalPath)
+
+    if(!avatar.url){
+        throw new ApiError(400, "Error while uploading avatar")
+    }
+
+    await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+})
+
+
+export { registerUser, logInUser, logOutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar }
 
 
 
